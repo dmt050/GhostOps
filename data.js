1dec58314933cd710890aed18e81e97285e5cfa7
dmt050 <dmarinotoso@gmail.com>
2026-04-30 17:34:05 -0300

    New Provinces
    Add ocoro and Agua Verde provinces.
    Some minor changes:
    implemented two debugs, one for open the app with an selected map choosen and another to get x,y map coords to make my life easier to create de POIs locations.

--- data.js
+++ data.js
@@ -43,6 +43,112 @@ const itacuaPOIs = [
   { name:"Observation Outpost (Unidad)",         type:"Military", x:95,  y:172 },
 ];
 
+const ocoroPOIS = [
+  // Fazendas
+  { name:"El Altiplano Verde",            type:"Safe",       x:425, y:103 },
+  // Checkpoints
+  { name:"CP 01 (South Ocoro)",           type:"Checkpoint",  x:353, y:91 },
+  { name:"CP 02 (Central Ocoro)",         type:"Checkpoint",  x:368, y:168 },
+  { name:"CP 03 (East Ocoro)",            type:"Checkpoint",   x:475, y:225, water:true },
+  // Vilas e POIs
+  { name:"Campo Ana",                     type:"Village",     x:159, y:51, water:true },
+  { name:"Pujio",                         type:"Village",     x:446, y:225, water:true },
+  { name:"Puerto Grande",                 type:"Village",     x:190, y:291 , water:true },
+  { name:"Puerto Nuevo",                  type:"Village",     x:202, y:161, water:true },
+  { name:"Gas Station Via-B 01",          type:"POI",         x:339, y:35 },
+  { name:"Gas Station Via-B 02",          type:"POI",         x:379, y:147 },
+  { name:"Moko",                          type:"POI",         x:215,  y:138  },
+  { name:"Santa Rosa",                    type:"POI",         x:369, y:46 },
+  { name:"Los Mosquitos",                 type:"POI",         x:327, y:275, water:true },
+  { name:"Pachama Scars",                 type:"POI",         x:298, y:66  },
+  { name:"Ocoro Dispensary",              type:"POI",         x:330, y:89 },
+  { name:"Nenma Freight Yard",            type:"POI",         x:279, y:164 },
+  // Seguros (inserção/extração apenas)
+  { name:"Oroco Alpha",               type:"Safe",       x:297, y:110 },
+  { name:"Oroco Bravo",               type:"Safe",       x:390, y:167 },
+  { name:"Oroco Charlie",             type:"Safe",       x:199, y:270 },
+  { name:"Oroco Delta",               type:"Safe",       x:201, y:144 },
+  { name:"A Fisherman's House",       type:"Safe",       x:179,  y:307, water:true },
+  // Santa Blanca — Military
+  { name:"Outpost 01 (SB-SouthWest)", type:"Military", x:200, y:63, prisoners:true },
+  { name:"Outpost 02 (SB-Central)",   type:"Military", x:364, y:131 },
+  { name:"Outpost 03 (SB-Pujio South Sector)",    type:"Military", x:427, y:196 },
+  { name:"Outpost 04 (SB-Pujio East Sector)",     type:"Military", x:472, y:219, prisoners:true },
+  { name:"Outpost 05 (SB-Pujio West Sector)",     type:"Military", x:424, y:233 },
+  { name:"Outpost 06 (SB-Chem Storage)",          type:"Military", x:328, y:362  },
+  { name:"Outpost 07 (SB- Prison Camp)",          type:"Military", x:267,  y:305, prisoners:true },
+  { name:"Outpost 08 (SB- West)",                 type:"Military", x:166,  y:254, prisoners:true },
+  { name:"Oroco Base (SB)",                       type:"Military", x:315, y:240, prisoners:true },
+  { name:"Coca Paste Factory (SB)",             type:"Military", x:248, y:372, prisoners:true },
+  { name:"Buchon House",                        type:"Military", x:249, y:343 },
+  { name:"Zona Narco Prohibida 01 (SB - Southwest)",   type:"Military", x:189, y:83, water:true },
+  { name:"Zona Narco Prohibida 02 (SB - Southwest)",   type:"Military", x:197, y:114, water:true },
+  { name:"Zona Narco Prohibida 03 (SB - West)",        type:"Military", x:167, y:168, water:true },
+  { name:"Zona Narco Prohibida 04 (SB - Northwest)",   type:"Military", x:158, y:211, water:true },
+  { name:"Zona Narco Prohibida 05 (SB - Northwest)",   type:"Military", x:143, y:254, water:true },
+  // Unidad — Military
+  { name:"Combat Outpost (Unidad)",                    type:"Military", x:224, y:240, water:true, prisoners:true },
+  
+];
+
+  const aguaverdePOIs = [
+    // Checkpoints
+  { name:"CP 01 (West Aguaverde)",           type:"Checkpoint", x:158, y:141 },
+  { name:"CP 02 (Riverine)",        type:"Checkpoint", x:325, y:200, water:true },
+  // Vilas e POIs
+  { name:"Goyo",                    type:"Village",    x:310, y:182, water:true },
+  { name:"Alicia Town",             type:"Village",    x:144, y:200, water:true },
+  { name:"Arroyo Baya",             type:"Village",    x:471, y:194, water:true },
+  { name:"Paraiso Casino",          type:"POI",        x:278, y:109 },
+  { name:"Paraíso Agua Verde",      type:"POI",        x:196, y:155 },
+  { name:"Playa Sirena",            type:"POI",        x:435, y:210, water:true },
+  { name:"La Nube Blanca",          type:"POI",        x:447, y:203, water:true },
+  { name:"Secret Cocaine Hideout",  type:"POI",        x:334, y:234, water:true },
+  { name:"Patuja Mansion",          type:"POI",        x:296, y:256, water:true },
+  { name:"Ajuga Mansion",           type:"POI",        x:287, y:281, water:true },
+  { name:"Kantuta Mansion",         type:"POI",        x:235, y:308, water:true },
+  { name:"Tajibo Mansion",          type:"POI",        x:216, y:308, water:true },
+  { name:"Playa Tajibo",            type:"POI",        x:154, y:270, water:true },
+  { name:"Rio del Jaguar",          type:"POI",        x:127, y:272, water:true },
+  // Seguros (inserção/extração apenas)
+  { name:"Agua Verde Alpha",            type:"Safe",       x:179, y:238, water:true },
+  { name:"Agua Verde Bravo",            type:"Safe",       x:451, y:240 },
+  { name:"Agua Verde Charlie",          type:"Safe",       x:292, y:178, water:true },
+  { name:"Agua Verde Delta",            type:"Safe",       x:212, y:86 },
+  { name:"La Torre de Arroyo Baya",     type:"Safe",       x:497, y:221 },
+  { name:"Siren Island",                type:"Safe",       x:223, y:270, water:true },
+  //Santa Blanca — Military
+  { name:"Outpost 01 (SB - South)",       type:"Military", x:249, y:68 },
+  { name:"Outpost 02 (SB - Central)",     type:"Military", x:346, y:225 },
+  { name:"Outpost 03 (SB - Northeast)",       type:"Military", x:494, y:267 },
+  { name:"Buchon House",       type:"Military", x:253, y:226, water:true },
+];  
+
+const PROVINCES = [
+  {
+    id: "itacua",
+    name: "Itacua",
+    mapImage: "maps/itacua_map.jpg",
+    bounds: [[0, 0], [400, 620]],
+    pois: itacuaPOIs
+  },
+  {
+    id: "ocoro",
+    name: "Ocoro",
+    mapImage: "maps/ocoro_map.jpg",
+    bounds: [[0, 0], [400, 620]],
+    pois: ocoroPOIS
+  },
+  {
+    id: "aguaverde",
+    name: "Agua Verde",
+    mapImage: "maps/aguaverde_map.jpg",
+    bounds: [[0, 0], [400, 620]],
+    pois: aguaverdePOIs
+  }
+
+];
+
 // ─── OPERADOR ROLES ──────────────────────────────────────────────
 const ROLES = {
   Sniper: {
